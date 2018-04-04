10501808379da5b8ed4baa53645e5e9b433d244f
Eder Soares <edersoares@me.com>
2018-04-04 20:51:41 -0300

    URL status

--- app/Url.php
+++ app/Url.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Url extends Model
 {
-    //
+    const STATUS_OPENED = 1;
+    const STATUS_NOTIFIED = 2;
+    const STATUS_CLOSED = 3;
+    const STATUS_QUARANTINE = 4;
+
+    public static function getStatusToCheck()
+    {
+        return [
+            self::STATUS_OPENED,
+            self::STATUS_NOTIFIED,
+            self::STATUS_QUARANTINE,
+        ];
+    }
 }
